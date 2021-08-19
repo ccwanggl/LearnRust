ec6c47cb7cba28d5dc2e51cd65dd61c7b4cf22a5
guoliang wang <tswanggl@hotmail.com>
2021-08-19 16:33:15 +0800

    Update message

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -12,7 +12,7 @@ fn main() {
         
         let mut guess = String::new();
         
-        io::stdin().read_line(&mut guess).expect("Can not read");
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
         
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
